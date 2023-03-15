b7aa95590fee897dd5ca387cc8e898af28075e97
YGKING <hrd18960706057@163.com>
2023-03-15 20:08:08 +0800

    0315

---
+++ src/day01/HelloWorld.java
@@ -0,0 +1,20 @@
+package day01;// 声明当前类所属包是package
+
+/**
+ * @ClassName HelloWorld
+ * @Description 控制台打印”Hello, World!“
+ * @Author YGKING  e-mail:hrd18960706057@163.com
+ * @Date 2023/03/15 16:47
+ * @Version 1.0
+ */
+// 声明类HelloWorld
+public class HelloWorld {
+    // 主方法：为程序的入口，程序的执行从main方法开始，main方法执行完程序结束
+    public static void main(String[] args) {
+        // 控制台打印”Hello, World!“
+        System.out.println("Hello, World!");
+        System.out.println("欢迎来到达内！");
+        System.out.println("蝙蝠再飞不是鸟，新鞋再好他不跟脚");
+        System.out.println("你好，我是YGKING");
+    }
+}
