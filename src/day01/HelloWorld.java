e7592e37b4654eaf273c6d556f5db667964ad4c5
YGKING <hrd18960706057@163.com>
2023-03-15 20:28:33 +0800

    删除readme文件

--- src/day01/HelloWorld.java
+++ src/day01/HelloWorld.java
@@ -15,6 +15,7 @@ public class HelloWorld {
         System.out.println("Hello, World!");
         System.out.println("欢迎来到达内！");
         System.out.println("蝙蝠再飞不是鸟，新鞋再好他不跟脚");
+        System.out.println("我可以笑着给你一个台阶下，也可以反手给你一个大嘴巴");
         System.out.println("你好，我是YGKING");
     }
 }
